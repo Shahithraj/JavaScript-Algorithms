d1955c9bdca65ad2de12fbc67a398b1bf88806c0
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2022-10-04 22:35:32 +0530

    finding n is prime and upto n numbers

--- MathAlgo/primeNumber.js
+++ MathAlgo/primeNumber.js
@@ -1,14 +1,14 @@
-// function isPrimeNumber(n) {
-//   if (n < 2) {
-//     return false;
-//   }
-//   for (let i = 2; i < n; i++) {
-//     if (n % i === 0) {
-//       return false;
-//     }
-//   }
-//   return true
-// }
+function isPrimeNumber(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true
+}
 
 //  Optimized
 function isPrimeNumber(n) {
@@ -27,3 +27,24 @@ function isPrimeNumber(n) {
 console.log(isPrimeNumber(1)); // false
 console.log(isPrimeNumber(5)); // true
 console.log(isPrimeNumber(4)); // false
+
+
+// 3. Finding prime numbers upto n number
+
+function sieveOfErathosthenes(n) {
+	   let isPrime = []
+     // make all numbers to true
+     for(let i=0;i<=n;i++) {
+       isPrime[i] = true
+     }
+   isPrime[0] = false;
+		isPrime[1] = false;
+  // finding the divisible numbers of i such as 2(4,6,8...),3(6,9,12),4,5,6,7 and make it false
+			   for(let i=2; i*i<=n;i++) {
+				   for(let j=2*i;j<=n;j+=i) {
+					  isPrime[j] = false;
+				   }
+			   }
+  isPrime.shift() // removing the 0th index
+  return isPrime;
+   }
