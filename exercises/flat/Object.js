1ef5077e53d4e3af70729f186f5111360296f278
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2025-05-18 21:20:13 +0530

    Update Object.js

--- exercises/flat/Object.js
+++ exercises/flat/Object.js
@@ -8,6 +8,25 @@ let obj = {
     }
 }
 
+// 1. In Single Function
+function flatObject(obj,oldKey) {
+   
+    let newObj = {};
+  for(let key in obj) {
+      if(typeof obj[key] != "object") {
+          
+          const newKey = oldKey ? `${oldKey}.${key}` : key;
+          newObj[newKey] = obj[key];
+      }
+      else {
+          const flattenObj = flatObject(obj[key], oldKey ? `${oldKey}.${key}` : key);
+          newObj = {...newObj, ...flattenObj};
+      }
+  }
+  return newObj;
+}
+
+// 2. With Helper function
 function flatObject(obj) {
     let newObj = {};
     const flat = (obj) => {
